680bc1aa29389409083cb004c7262390f719bbe2
Sean Beard <sean61793@my.uri.edu>
2020-10-31 19:20:43 -0600

    feat(Home): render feed items

---
+++ src/components/FeedItemCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <button class="card-btn" @click="handleClick">
+      <p>{{ item.title }}</p>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeedItemCard",
+  props: {
+    item: Object
+  },
+  methods: {
+    handleClick() {
+      this.$emit("feed-item-clicked", this.item.guid);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid black;
+}
+
+.card-btn {
+  width: 100%;
+  cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,14 +14,40 @@
         </a>
       </div>
     </nav>
+
+    <div class="layout">
+      <section>
+        <div v-for="item in feedItems" :key="item.guid">
+          <FeedItemCard
+            :item="item"
+            v-on:feed-item-clicked="handleFeedItemClick"
+          />
+        </div>
+      </section>
+      <section
+        class="item-desc"
+        v-if="!!selectedDesc"
+        v-html="selectedDesc"
+      ></section>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from "vuex";
+import FeedItemCard from "@/components/FeedItemCard";
 
 export default {
   name: "Home",
+  components: {
+    FeedItemCard
+  },
+  data() {
+    return {
+      feedItems: [],
+      selectedDesc: ""
+    };
+  },
   methods: {
     ...mapActions(["setIsAuthenticated"]),
     logout() {
@@ -32,6 +58,27 @@ export default {
             this.setIsAuthenticated({ isAuthenticated: false, token: "" });
           }
         });
+    },
+    getFeeds() {
+      fetch("http://localhost:4001/feed", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.authToken}`
+        }
+      })
+        .then(r => r.json())
+        .then(feeds => {
+          const feedItems = feeds.map(({ items }) => items);
+          // TODO: flatten this
+          this.feedItems = feedItems[0];
+        });
+    },
+    handleFeedItemClick(guid) {
+      const item = this.feedItems.find(item => item.guid === guid);
+
+      if (item) {
+        this.selectedDesc = item.description;
+      }
     }
   },
   computed: {
@@ -39,16 +86,7 @@ export default {
   },
   created() {
     if (this.isAuthenticated) {
-      fetch("http://localhost:4001/subscription", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${this.authToken}`
-        }
-      })
-        .then(r => r.json())
-        .then(data => {
-          console.log(data);
-        });
+      this.getFeeds();
     }
   }
 };
@@ -60,5 +98,13 @@ nav {
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
+}
+
+.layout {
+  display: flex;
+}
+
+.item-desc {
+  flex: 2;
 }
 </style>
