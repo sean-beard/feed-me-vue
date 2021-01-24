5f90c020a239ea3415879a002d87d15802a1721d
Sean Beard <sean61793@my.uri.edu>
2021-01-23 23:25:15 -0700

    refactor(api): extract requestWithBody helper

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,9 +13,15 @@ export const get = async (path, opts = { useAuth: true }) => {
   return response.json();
 };
 
-export const post = async (path, body) => {
+export const post = async (path, body) =>
+  requestWithBody({ method: "POST", path, body });
+
+export const deletion = async (path, body) =>
+  requestWithBody({ method: "DELETE", path, body });
+
+const requestWithBody = async ({ method, path, body }) => {
   const response = await fetch(`${process.env.VUE_APP_BASE_API_URL}${path}`, {
-    method: "POST",
+    method,
     body: JSON.stringify(body),
     headers: {
       "Content-Type": "application/json",
@@ -26,17 +32,4 @@ export const post = async (path, body) => {
   return response.status !== 200
     ? { status: response.status }
     : response.json();
-};
-
-export const deletion = async (path, body) => {
-  const response = await fetch(`${process.env.VUE_APP_BASE_API_URL}${path}`, {
-    method: "DELETE",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${store.getters.authToken}`
-    }
-  });
-
-  return response.json();
 };
