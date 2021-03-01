6d9bee226fa0cfc307af2d3ec60d0b2fc9f72171
Sean Beard <sean61793@my.uri.edu>
2021-02-28 18:28:20 -0700

    feat(AudioPlayer): support ability to rewind/fast forward

--- src/App.vue
+++ src/App.vue
@@ -76,4 +76,15 @@ footer {
   background-color: white;
   color: #1a237e;
 }
+
+.visually-hidden {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
 </style>
